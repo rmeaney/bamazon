a1e945b19639363909f6675afb53245417943f8c
rmeaney <rjmeaney@gmail.com>
2017-08-09 21:02:22 -0400

    functionality for part 2 completed, some glitches remain

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 var items = [];
+var smallItems = [];
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -16,7 +17,7 @@ connection.connect(function(err){
     }
     console.log("Connected as id: " + connection.threadId);
     grabItems();
-    startMenu();
+    //startMenu();
 
 });
 
@@ -24,8 +25,12 @@ function grabItems(){
 	connection.query('SELECT * FROM bamazonitems', function(err, result){
 		for(i = 0; i < result.length; i++){
 			items.push(result[i]);
+			smallItems.push(result[i].item_id + ' ' + result[i].product_name + ' ' + result[i].stock_quantity);
+
 		}
+
 	});
+	startMenu();
 }
 function startMenu(){
 	inquirer.prompt([
@@ -33,7 +38,7 @@ function startMenu(){
 			name: 'startChoice',
 			type: 'rawlist',
 			message: 'Welcome, what would you like to do.',
-			choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product']
+			choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product', 'Quit']
 		}
 	]).then(function(result){
 		var menuChosen = result.startChoice;
@@ -50,6 +55,10 @@ function startMenu(){
 			case 'Add New Product':
 				addProduct();
 				break;
+			case 'Quit':
+				console.log('Have a Wonderful Day!');
+				connection.end();
+				break;
 		}
 
 	})
@@ -60,6 +69,7 @@ function showItems(){
 		console.log(items[i].item_id + ' ' + '|' + items[i].product_name + ' |' + ' department: ' + items[i].department_name + ' |' + ' Price: $' + items[i].price + ' |' + ' Quantity: ' + items[i].stock_quantity);
 		console.log('__________________________________________________________________________');
 	}
+	anotherTransaction();
 }
 
 function lowInventory(){
@@ -70,12 +80,92 @@ function lowInventory(){
 			console.log('________________________________________________________________________');
 		}
 	}
+	anotherTransaction();
 }
 
 function addInventory(){
 	console.log('Add Inventory');
+	inquirer.prompt([
+	{
+		type: 'list',
+		name: 'selectItem',
+		message: 'Please select an item that you would like to update.',
+		choices: smallItems
+	},{
+		type: 'input',
+		name: 'numToAdd',
+		message: 'Please enter the amount you would like to add (numbers only, please).'
+	}
+	]).then(function(result){
+		var chosenItem = items[result.selectItem[0] - 1].stock_quantity;
+			//console.log(chosenItem);
+		//console.log(items[result.selectItem[0] - 1].stock_quantity);
+		var newQuantity = parseInt(result.numToAdd) + parseInt(chosenItem);
+		var tableItem = items[result.selectItem[0] - 1];
+		
+		connection.query("UPDATE bamazonitems set stock_quantity = " + newQuantity + ' WHERE item_id = ' + tableItem.item_id, function(err, result){
+				
+					if(err) throw err;
+					
+					console.log(tableItem.product_name + ' Has been updated. Currently ' + newQuantity + ' in stock.')
+				
+					anotherTransaction();
+				})
+
+	});
 }
 
 function addProduct(){
-	console.log('Add Product');
+	inquirer.prompt([
+			{
+				type: 'input',
+				name: 'newItemName',
+				message: 'Please enter the name of your new item.'
+			},
+			{
+				type: 'input',
+				name: 'newItemDepartment',
+				message: 'Please enter the category of your new item.'
+			},
+			{
+				type: 'input',
+				name: 'newItemPrice',
+				message: 'Please enter the price of your new item.'
+			},
+			{
+				type: 'input',
+				name: 'newItemQuantity',
+				message: 'Please enter the starting quantity of the new item.'
+			}
+		]).then(function(answer){
+			var sql = "INSERT INTO bamazonitems (product_name, department_name, price, stock_quantity) VALUES (?, ?, ?, ?)";
+			var inserts = [answer.newItemName, answer.newItemDepartment, answer.newItemPrice, answer.newItemQuantity];
+			preparedSqlQuery = mysql.format(sql, inserts);
+			console.log(preparedSqlQuery);
+			connection.query(preparedSqlQuery, function(err, result){
+				if(err) throw err;
+				console.log(answer.newItemName + ' has been added to the our database! It is now available to be viewed, and edited');
+				anotherTransaction();
+			});
+		});
 }
+
+function anotherTransaction(){
+		inquirer.prompt([
+			{
+				type:'list',
+				name:'startOver',
+				message: 'Would you like to make another Transaction?',
+				choices:['Yes', 'No']
+			}
+			]).then(function(result){
+				if(result.startOver === 'Yes'){
+					smallItems =[];
+					items =[];
+					grabItems();
+				}else{
+					console.log('Have a wonderful day!');
+					connection.end();
+				}
+			});
+	}
